61aad229578475214f6deee98f78387580676927
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-21 22:13:43 +0100

    feat: added toast for better ui

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { supabase } from "./supabaseClient";
 
 export const register = async(email, password) => {
@@ -30,7 +31,20 @@ export const logout = async () => {
 export const getUser = async () => {
   const {data: { user }, error} = await supabase.auth.getUser();
   if (error) {
-    throw new Error(error.message);
+    console.log(error.message)
+    toast("Error fetching user data at the moment....")
   }
   return user;
 };
+
+// Password reset
+export const resetPassword = async (email, id) => {
+  const { data, error } = await supabase.auth.resetPasswordForEmail({
+    email,
+  })
+
+  if (error) {
+    toast("Error fetching user data at the moment....");
+  }
+  return data
+}
