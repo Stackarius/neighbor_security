4a568c063df08a4f30b6dd62287f2b3dd7568a69
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-21 21:33:57 +0100

    feat: designed dashboard takes sidebar comp as child

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,15 @@
+
+import Sidebar from "../../component/Sidebar";
+
+export const metadata = {
+  title: "Dashboard",
+};
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-4">{children}</main>
+    </div>
+  );
+}
