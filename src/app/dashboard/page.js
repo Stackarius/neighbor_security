d63fe6f30551ebd4d210f4f3045b1e05dd2f9c7a
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-21 21:35:53 +0100

    feat: design dashboard page with reportList and reportForm, shows based on userSession

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect , useState} from "react";
+import { useRouter } from "next/navigation";
+import { getUser } from "../../lib/auth"; 
+import { supabase } from "../../lib/supabaseClient";
+import Reports from "../../component/Reports";
+import ReportForm from "../../component/ReportForm";
+import ReportList from "../../component/ReportList";
+
+export default function Dashboard() {
+  const router = useRouter();
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await getUser();
+      if (!user) {
+        router.push("/login");
+      }
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', user.id)
+        .single();
+      if (error) {
+        console.error("Error fetching profile:", error);
+        return;
+      }
+      setProfile(data);
+    };
+    checkAuth();
+  }, [router]);
+
+  return (
+    <div className="flex flex-col p-6 max-h-screen overflow-y-auto">
+        <h2 className="text-3xl font-bold mb-4">Welcome to Your Dashboard</h2>
+        <p className="text-gray-700">
+          Here you can monitor reports and manage your profile.
+        </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+        <ReportList user={profile} />
+        <ReportForm user={profile} />
+      </div>
+    </div>
+  );
+}
