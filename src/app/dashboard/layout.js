1059c762fe28d8be678dd7408a2e2e09b3dd5bcd
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-28 02:36:52 +0100

    feat: removed metadata, added sidebar for mobile

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,15 +1,21 @@
+"use client";
+
 import Sidebar from "@/component/Sidebar";
-
-
-export const metadata = {
-  title: "Dashboard",
-};
+import { LucideMenu } from "lucide-react";
+import { useState } from "react";
 
 export default function DashboardLayout({ children }) {
-  return (    
-      <div className="flex min-h-screen">
-      <Sidebar />
-        <main className="flex-1 p-4">{children}</main>
-      </div>
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <div className="flex min-h-screen">
+      {!isOpen && <Sidebar />}
+      {/* Show Nav icon on sm dev */}
+      <LucideMenu
+        className="block fixed top-4 right-4 text-blue-600"
+        onClick={() => setOpen(!isOpen)}
+      />
+      <main className="flex-1 p-4">{children}</main>
+    </div>
   );
 }
