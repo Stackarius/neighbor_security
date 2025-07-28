fec9e4d10439faa8a94aebb8decc3df7fcb160b2
Adepoju Opeyemi <opepoju@gmail.com>
2025-07-28 02:39:24 +0100

    feat: trying to refresh after logout

--- src/component/LogoutButton.js
+++ src/component/LogoutButton.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { LogOutIcon } from 'lucide-react'
-import { logout } from '@/lib/auth'
-import { toast } from 'react-toastify'
+import React, { useEffect } from "react";
+import { LogOutIcon } from "lucide-react";
+import { logout } from "@/lib/auth";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const LogoutButton = () => {
+  const router = useRouter();
+  const handleLogout = async () => {
+    await logout();
+    router.refresh()
+  };
 
-    const handleLogout = async () => {
-        await logout()
-    }
   return (
     <button
-            onClick={() => {
-              handleLogout()
-              toast.success("Logout successful");
-            }}
-            className="inline-flex text-white items-center font-bold px-2 py-1 bg-red-600 rounded hover:bg-red-700 text-center"
-          >
-            <LogOutIcon className="h-4 w-4 mr-3" /> Logout
-          </button>
-  )
-}
+      onClick={() => {
+        handleLogout();
+        toast.success("Logout successful");
+        router.refresh()
+      }}
+      className="inline-flex text-white items-center font-bold px-2 py-1 bg-red-600 rounded hover:bg-red-700 text-center"
+    >
+      <LogOutIcon className="h-4 w-4 mr-3" /> Logout
+    </button>
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
