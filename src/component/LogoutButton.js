16e22b0d1d68e90f94a5bbc83c1593308e11b9f9
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-05 02:10:22 +0100

    feat: handle redirect wthout flashing page

--- src/component/LogoutButton.js
+++ src/component/LogoutButton.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { LogOutIcon } from "lucide-react";
 import { logout } from "@/lib/auth";
 import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
+import { useRouter, redirect } from "next/navigation";
 
 const LogoutButton = () => {
   const router = useRouter();
@@ -16,7 +16,7 @@ const LogoutButton = () => {
       onClick={() => {
         handleLogout();
         toast.success("Logout successful");
-        router.refresh()
+        redirect("/login");
       }}
       className="inline-flex text-white items-center font-bold px-2 py-1 bg-red-600 rounded hover:bg-red-700 text-center"
     >
