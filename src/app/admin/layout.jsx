cde57315bb730ef9637040f1654ad068c1b41320
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-10 18:50:15 +0100

    feat: customize lucideMenu button

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({ children }) {
       {!isOpen && <AdminBar />}
       {/* Show Nav icon on sm dev */}
       <LucideMenu
-        className="block fixed top-4 right-4 text-blue-600"
+        className="block fixed top-4 bg-blue-700 text-white right-4 text-blue-600 z-30 shadow rounded p-1 cursor-pointer"
         onClick={() => setOpen(!isOpen)}
       />
       <main className="flex-1 h-full">{children}</main>
