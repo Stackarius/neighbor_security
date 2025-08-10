b94b04d4e01f3825dba80e3aec347eb5c2f59ca8
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-10 19:28:57 +0100

    feat: added users page to sidebar

--- src/component/AdminBar.jsx
+++ src/component/AdminBar.jsx
@@ -6,12 +6,13 @@ import { usePathname } from "next/navigation";
 import LogoutButton from "./LogoutButton";
 import { motion } from "framer-motion";
 
-export default function Sidebar() {
+export default function AdminBar() {
     const pathname = usePathname();
 
     const navItems = [
         { name: "Home", href: "/admin" },
         { name: "All Reports", href: "/admin/reports" },
+        { name: "Manage Users", href: "/admin/users" },
     ];
 
     return (
@@ -20,15 +21,16 @@ export default function Sidebar() {
             animate={{ opacity: 1, x: 0 }}
             transition={{ ease: "anticipate", duration: 0.5 }}
 
-            className="fixed w-60 h-[100vh] bg-gray-900 shadow-md md:shadow-none  text-white p-4 space-y-4 md:block md:relative"
+            className="fixed w-60 h-[100vh] bg-gray-900 shadow-md md:shadow-none  text-white p-4 space-y-4 md:block md:relative z-30"
         >
-            <h2 className="text-xl font-bold mb-6">Security Watch</h2>
+            <h2 className="text-xl font-bold mb-6">NSW</h2>
+            <h2 className="text-xl font-bold mb-6">Dashboard</h2>
             <ul className="space-y-2">
                 {navItems.map((item) => (
                     <li key={item.name}>
                         <Link
                             href={item.href}
-                            className={`block px-2 font-semibold py-1 rounded hover:bg-blue-700 ${pathname === item.href ? "bg-gray-700" : ""
+                            className={`block px-2 font-semibold py-1 rounded hover:bg-gray-700 ${pathname === item.href ? "bg-blue-700" : ""
                                 }`}
                         >
                             {item.name}
