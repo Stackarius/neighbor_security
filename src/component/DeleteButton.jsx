3e32f325db10d95a7d8e232edba790f10d210d45
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-12 16:50:37 +0100

    fix: reduce button size

--- src/component/DeleteButton.jsx
+++ src/component/DeleteButton.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DeleteButton = ({click}) => {
     return (
         <button onClick={click} className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 transition-colors duration-200">
-            <Trash2 className='cursor-pointer inline mr-1'/> Delete
+            <Trash2 className='cursor-pointer inline mr-1' size={15}/> Delete
       </button>
   )
 }
