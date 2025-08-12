7f1f4ab5f30e04c651eb45ab14ed6d1a38903645
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-12 16:49:34 +0100

    fix: menu bar toggle options

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import AdminBar from "@/component/AdminBar";
-import { LucideMenu } from "lucide-react";
+import { LucideMenu, LucideX } from "lucide-react";
 import { useState } from "react";
 
 export default function DashboardLayout({ children }) {
@@ -11,10 +11,10 @@ export default function DashboardLayout({ children }) {
       <div className="flex min-h-screen overflow-y-auto bg-gray-100">
       {!isOpen && <AdminBar />}
       {/* Show Nav icon on sm dev */}
-      <LucideMenu
+      {isOpen ? <LucideMenu
         className="block fixed top-4 bg-blue-700 text-white right-4 text-blue-600 z-30 shadow rounded p-1 cursor-pointer"
         onClick={() => setOpen(!isOpen)}
-      />
+      /> : <LucideX onClick={() => setOpen(!isOpen)} className="block fixed top-4 bg-blue-700 text-white right-4 text-blue-600 z-30 shadow rounded p-1 cursor-pointer" />}
       <main className="flex-1 h-full">{children}</main>
     </div>
   );
