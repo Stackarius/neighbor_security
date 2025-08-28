75a0cc92be3eaaf8b9bdbaccb66f2e42c6b480d1
Adepoju Opeyemi <opepoju@gmail.com>
2025-08-28 17:35:01 +0100

    feat: added close button when menu is opened

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -3,6 +3,7 @@
 import Sidebar from "@/component/Sidebar";
 import { LucideMenu } from "lucide-react";
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
 export default function DashboardLayout({ children }) {
   const [isOpen, setOpen] = useState(false);
@@ -11,10 +12,14 @@ export default function DashboardLayout({ children }) {
     <div className="flex min-h-screen">
       {!isOpen && <Sidebar />}
       {/* Show Nav icon on sm dev */}
-      <LucideMenu
-        className="block fixed top-4 right-4 text-blue-600"
-        onClick={() => setOpen(!isOpen)}
-      />
+      {isOpen ?
+        <LucideMenu
+          className="block fixed top-4 right-4 text-blue-600"
+          onClick={() => setOpen(!isOpen)}
+        />
+        : <FaTimes className="block fixed top-4 right-4 text-blue-600"
+          onClick={() => setOpen(!isOpen)} />
+      }
       <main className="flex-1 p-4">{children}</main>
     </div>
   );
